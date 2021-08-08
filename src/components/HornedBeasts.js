b762fb35eb7a80f1ee556d1015eccd305068bd43
Yazeed Aloufee <yazeedaloufee@gmail.com>
2021-08-08 19:13:46 +0300

    solved lab

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -6,7 +6,7 @@ class HornedBeasts extends React.Component {
         return (
             <>
                 <h2>{this.props.title}</h2>
-                <img src='' alt='' title=''>{this.props.imageUrl}</img>
+                <img src={this.props.imageUrl} alt='' title=''></img>
                 <p>{this.props.description}</p>
             </>
         )
