9590201854cbd948ca63c5a3dd9efdfb31ce1400
Yazeed Aloufee <yazeedaloufee@gmail.com>
2021-08-09 17:29:31 +0300

    lab completed

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,13 +1,44 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+    increaseCounter = () => {
+        this.setState({ counter: this.state.counter + 1 })
+
+
+    }
+
+
+
     render() {
         console.log(this.props.imageUrl);
         return (
             <>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.imageUrl} alt='' title=''></img>
+                {/* <h2>{this.props.title}</h2>
+                <img  src={this.props.imgUrl} alt='' title=''></img>
                 <p>{this.props.description}</p>
+                <p>favorite by {this.state.counter}</p> */}
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.imgUrl} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                        {this.props.description}
+                        <p>favoutie by {this.state.counter} people</p>
+                        </Card.Text>
+                        <Button onClick={this.increaseCounter}variant="primary">vote a favorite</Button>
+                    </Card.Body>
+                </Card>
             </>
         )
 
